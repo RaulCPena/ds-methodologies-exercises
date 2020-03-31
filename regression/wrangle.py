dfd669c3d094746f2964e9b4c7552cdb5aff3db6
RaulCPena <raul.c.pena@gmail.com>
2020-03-31 16:14:41 -0500

    modified file system

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -11,6 +11,7 @@ from env import host, user, password
 
 
 # function to get url
+
 def get_db_url(db_name):
     return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
 
@@ -25,9 +26,16 @@ def get_data_from_sql():
     df = pd.read_sql(query, get_db_url('telco_churn'))
     return df
 
+# function that rules them all by acquiring and prepping my df for exploration or modeling
 
-
-
-
-
-
+def wrangle_telco():
+    """
+    Queries the telco_churn database
+    Returns a clean df with four columns:
+    customer_id(object), monthly_charges(float), tenure(int), total_charges(float)
+    """
+    df = get_data_from_sql()
+    df.tenure.replace(0, 1, inplace=True)
+    df.total_charges.replace(' ', df.monthly_charges, inplace=True)
+    df.total_charges = df.total_charges.astype(float)
+    return df
