e850c99874b6e0a5b90c94996cb6c3efbb95ab7e
RaulCPena <raul.c.pena@gmail.com>
2020-04-14 17:13:29 -0500

    modified a function to work with data

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -16,26 +16,36 @@ def get_db_url(db_name):
     return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
 
 # function that passes my query and my url to return df
-
-def get_data_from_sql():
+def get_zillow_data_from_sql():
     query = '''
-    SELECT customer_id, monthly_charges, tenure, total_charges
-    FROM customers
-    WHERE contract_type_id = 3;
+    SELECT
+    bathroomcnt AS bathroom_count,
+    bedroomcnt AS bedroom_count,
+    calculatedfinishedsquarefeet AS square_feet,
+    -- fips AS fips_number,
+    -- ptype.propertylandusedesc,
+    taxvaluedollarcnt AS home_value
+    FROM properties_2017 AS prop
+    JOIN
+    predictions_2017 AS pred
+    ON prop.parcelid = pred.parcelid
+    JOIN
+    propertylandusetype AS ptype
+    ON prop.propertylandusetypeid = ptype.propertylandusetypeid
+
+WHERE transactiondate
+BETWEEN '2017-05-01' AND '2017-06-30' AND propertylandusedesc = 'Single Family Residential'
+
     '''
-    df = pd.read_sql(query, get_db_url('telco_churn'))
+    df = pd.read_sql(query, get_db_url('zillow'))
     return df
 
-# function that rules them all by acquiring and prepping my df for exploration or modeling
-
-def wrangle_telco():
-    """
-    Queries the telco_churn database
-    Returns a clean df with four columns:
-    customer_id(object), monthly_charges(float), tenure(int), total_charges(float)
-    """
-    df = get_data_from_sql()
-    df.tenure.replace(0, 1, inplace=True)
-    df.total_charges.replace(' ', df.monthly_charges, inplace=True)
-    df.total_charges = df.total_charges.astype(float)
+# function to clean up my zillow dataframe
+def prep_zillow(df):
+    df = df.dropna()
+    df['fips'] = df['fips'].astype(int)
+    # df['propertylandusedesc'] = df['propertylandusedesc'].astype('category')
+    df['square_feet'] = df['square_feet'].astype('int') # this is a string
+    # df['propertylandusetypeid'] = df['propertylandusetypeid'].astype(int)
     return df
+
