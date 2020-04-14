2a7fa647364e0ab0ff218ec061c2219181612df5
RaulCPena <raul.c.pena@gmail.com>
2020-04-14 17:04:07 -0500

    new file to house lesson on classification during methodologies 1

---
+++ classification/acquire.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import env
+
+def get_db_url(dbname) -> str:
+    url = 'mysql+pymysql://{}:{}@{}/{}'
+    return url.format(env.user, env.password, env.host, dbname)
+
+# Function for eaxercise
+def get_titanic_data():
+    return pd.read_sql('SELECT * FROM passengers', get_db_url('titanic_db'))
+
+
+
+# get iris data 
+def get_iris_data():
+    sql_query = """
+    SELECT species_id,
+    species_name,
+    sepal_length,
+    sepal_width,
+    petal_length,
+    petal_width
+    FROM measurements
+    JOIN species
+    USING(species_id)
+    """
+    return pd.read_sql(sql_query, get_db_url('iris_db'))
+
